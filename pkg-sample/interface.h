b10a3ed36d04fa55a793fb7c47019c0c3dc77473
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-06-12 15:04:46 -0400

    x

--- pkg-sample/interface.h
+++ pkg-sample/interface.h
@@ -2,10 +2,18 @@
 #include <sys/types.h>
 
 /* external interfaces */
-ssize_t read(int fd, void *buf, size_t count);
+ssize_t read(int fd, void *buffer, size_t count);
+int printf(const char *format, ...);
 
 /* exposed interfaces */
 inline __attribute__((always_inline))
-ssize_t read_stdin(void *buf, size_t count) {
-    return read(0, buf, count);
+ssize_t in(void *buffer, size_t count) {
+    return read(0, buffer, count);
 }
+
+inline __attribute__((always_inline))
+int out(const char *message) {
+    return printf("%s", message);
+}
+
+void abort(void);
--- pkg-sample/main.c
+++ pkg-sample/main.c
@@ -1,5 +1,13 @@
 #include "interface.h"
 
 int main(void) {
-  return 0;
+    char input[10] = {0};
+    int len = in(input, 10);
+    if (len > 0) {
+        if (input[0] == 'a') {
+            abort();
+        }
+    }
+    out(input);
+    return 0;
 }
